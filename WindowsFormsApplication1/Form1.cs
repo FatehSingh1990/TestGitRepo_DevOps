98557e01f77801aff2124aa98907d46b5cb48699
FatehSingh <fathsingh1990@gmail.com>
2023-02-11 12:40:48 +0530

    Test

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 0)
+            {
+                MessageBox.Show("Click");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            char c='\u0049';
+            MessageBox.Show(""+c);
+        }
+    }
+}
