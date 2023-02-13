d916163a55912cf7c6df41e4a60734e977fac4ba
FatehSingh <fathsingh1990@gmail.com>
2023-02-13 12:59:54 +0530

    hhh

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -22,7 +22,7 @@ namespace WindowsFormsApplication1
             if (e.ColumnIndex == 0)
             {
                 MessageBox.Show("Click");
-                MessageBox.Show("Click")
+                MessageBox.Show("Click") 
             }
 
         }
